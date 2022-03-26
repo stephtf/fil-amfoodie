edede908cffc3b1ec9f84aff65a23a83909e21b6
Stephanie Fajardo <stfajardo@gmail.com>
2022-03-26 18:05:06 -0400

    update github with correct string

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://stephtf:cocousesaws@cluster0.hvqje.mongodb.net/fil-amfoodblog?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
