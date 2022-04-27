9f962369784d457a1a32f5f3542ab3858b8e0944
Stephanie Fajardo <stfajardo@gmail.com>
2022-04-27 17:54:53 -0400

    add adobo image

--- client/src/components/header/HamburgerLinks.js
+++ client/src/components/header/HamburgerLinks.js
@@ -6,13 +6,13 @@ const HamburgerLinks = ({ handleButtonClick, currentPage, handlePageChange }) =>
         <div className='hamlink-container'> 
             <ul>
                 <li className='nav-styles'>
-                    {currentPage==='Home' ? <NavLink to='/' className='ham-link' onClick={handleButtonClick}>Home</NavLink> : <NavLink to='/' className='ham-link' onClick={() => handlePageChange('Home')}>Home</NavLink> }
+                    {currentPage ==='Home' ? <NavLink to='/' className='ham-link' onClick={handleButtonClick}>Home</NavLink> : <NavLink to='/' className='ham-link' onClick={() => handlePageChange('Home')}>Home</NavLink> }
                 </li>
                 <li className='nav-styles'>
-                    {currentPage==='Recipes' ? <NavLink to='/recipes' className='ham-link' onClick={handleButtonClick}>Recipes</NavLink> : <NavLink to='/recipes' className='ham-link' onClick={() => handlePageChange('Recipes')}>Recipes</NavLink>}                    
+                    {currentPage ==='Recipes' ? <NavLink to='/recipes' className='ham-link' onClick={handleButtonClick}>Recipes</NavLink> : <NavLink to='/recipes' className='ham-link' onClick={() => handlePageChange('Recipes')}>Recipes</NavLink>}                    
                 </li>
                 <li className='nav-styles'>
-                    {currentPage==='About' ? <NavLink className='ham-link' to='/about' onClick={handleButtonClick}>About</NavLink> : <NavLink className='ham-link' to='/about' onClick={() => handlePageChange('About')}>About</NavLink>}
+                    {currentPage ==='About' ? <NavLink className='ham-link' to='/about' onClick={handleButtonClick}>About</NavLink> : <NavLink className='ham-link' to='/about' onClick={() => handlePageChange('About')}>About</NavLink>}
                 </li>
             </ul>
         </div>
--- client/src/components/pages/recipe-pages/individual-recipes/ChickenAdobo.js
+++ client/src/components/pages/recipe-pages/individual-recipes/ChickenAdobo.js
@@ -3,7 +3,7 @@ const ChickenAdobo = () => {
         <div>
             <h2 className='space-below'>Chicken Adobo</h2>
             <h3>A Filipino staple</h3>
-            <img className='main-image' src='recipe-photos/not-adobo.jpg' alt='a sushi bowl'/>
+            <img className='main-image' src='recipe-photos/adobo.jpg' alt='a sushi bowl'/>
         </div>
     )
 }
