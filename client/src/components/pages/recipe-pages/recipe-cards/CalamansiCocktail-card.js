fedecb25b72c2fb07b5e39511a983d41c2002212
Stephanie Fajardo <stfajardo@gmail.com>
2022-05-06 13:02:40 -0400

    update recipe card containers

--- client/src/components/pages/recipe-pages/recipe-cards/CalamansiCocktail-card.js
+++ client/src/components/pages/recipe-pages/recipe-cards/CalamansiCocktail-card.js
@@ -7,7 +7,7 @@ const CalamansiCocktailCard = () => {
         <div> 
             <Link to='/recipes/calamansi-cocktail'>
             <div className='recipe-cards-container space-below-25'>
-                <div className='space-below-10 card-title'>Calamansi Cocktail<br></br> <small>AKA "Madaling Malasing"</small></div>
+                <div className='space-below-10 card-title'>Calamansi Cocktail</div>
                 <img src={calamansi} className='card-image' alt='two glasses with calamansi cocktail and metal straws'/>
                 <button>View Recipe</button>
             </div>
