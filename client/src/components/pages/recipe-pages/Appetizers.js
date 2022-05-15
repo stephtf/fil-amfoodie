b63daeb1253eb0be16a3fd408ebfa4bf312a3fea
Stephanie Fajardo <stfajardo@gmail.com>
2022-05-15 17:05:31 -0400

    saving slider buttons

--- client/src/components/pages/recipe-pages/Appetizers.js
+++ client/src/components/pages/recipe-pages/Appetizers.js
@@ -4,20 +4,52 @@ import { useRef, useState } from 'react';
 
 const Appetizers = () => {
    
-    const [slideNumber, setSlideNumber] = useState(0);
+    // const [slideNumber, setSlideNumber] = useState(0);
 
     const listRef = useRef();
 
+    const [slideNumber, setSlideNumber] = useState(0);
+    // const [distance, setDistance] = useState('');
+    // const handleDistance = (setDistance(listRef.current.getBoundingClientRect().width)) => ()
+
     const handleArrowClick = (direction) => {
-        let distance = listRef.current.getBoundingClientRect().x;
-        if(direction === "left" && slideNumber > 0) {
+        // console.log(listRef);
+        let distance = listRef.current.getBoundingClientRect().width;
+        let N = listRef.current.childNodes.length;
+        // console.log(N)
+        let curr_x = listRef.current.getBoundingClientRect().x;
+        // let curr_right = listRef.current.getBoundingClientRect().right;
+
+        
+        // console.log(distance);
+        console.log('starting as ' + slideNumber);
+        
+        if(direction === "left" && curr_x < 0) {
+            // let move_distance = Math.min(curr_x, distance/N);
+            
+            listRef.current.style.transform = `translateX(${(-slideNumber*distance/N)+distance/N}px)`;
+            // console.log(listRef.current.style.transform);
+            // console.log(listRef.current.getBoundingClientRect().x)
             setSlideNumber(slideNumber - 1);
-            listRef.current.style.transform = `translateX(${100 + distance}px)`;
+
+            console.log('ending as ' + slideNumber);
+
+            
         }
-        if(direction === "right" && slideNumber < 10) {
-            setSlideNumber(slideNumber + 1);
-            listRef.current.style.transform = `translateX(${-100 - distance}px)`;
+        if(direction === "right" && (slideNumber < N)) {
+            let newslide = slideNumber + 1;
+            setSlideNumber(newslide);
+            console.log('ending as ' + slideNumber);
+            // console.log(slideNumber);
+            // let move_distance = Math.min(curr_right - (curr_right - curr_x),distance/N)
+            listRef.current.style.transform = `translateX(${-slideNumber*distance/N}px)`;
+            // console.log(listRef.current.style.transform);
+            // console.log(listRef.current.getBoundingClientRect().x)
         }
+
+        // console.log(distance/N)
+
+        
     }
 
     return (
