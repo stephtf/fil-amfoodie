ec6022d780f3f2a8d0100f3db86830915167e75d
Stephanie Fajardo <stfajardo@gmail.com>
2022-05-15 14:54:27 -0400

    add slider with distance

--- client/src/components/pages/recipe-pages/Appetizers.js
+++ client/src/components/pages/recipe-pages/Appetizers.js
@@ -1,25 +1,30 @@
 import LumpiangShanghaiCard from "./recipe-cards/LumpiangShanghai-card";
 import { ArrowBackIosOutlined, ArrowForwardIosOutlined } from '@mui/icons-material';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 
 const Appetizers = () => {
    
+    const [slideNumber, setSlideNumber] = useState(0);
+
     const listRef = useRef();
 
     const handleArrowClick = (direction) => {
-        let distance = listRef.current.getBoundingClientRect().x - 50;
-        if(direction === "right") {
+        let distance = listRef.current.getBoundingClientRect().x;
+        if(direction === "left" && slideNumber > 0) {
+            setSlideNumber(slideNumber - 1);
             listRef.current.style.transform = `translateX(${100 + distance}px)`;
         }
-        if(direction === "left") {
-            listRef.current.style.transform = `translateX(${100 - distance}px)`;
+        if(direction === "right" && slideNumber < 10) {
+            setSlideNumber(slideNumber + 1);
+            listRef.current.style.transform = `translateX(${-100 - distance}px)`;
         }
     }
 
     return (
         <div className='content-container'>
-            <div className='recipes-wrapper'>
+            <div className='arrow-container'>
             <ArrowBackIosOutlined onClick={() => handleArrowClick("left")} /> 
+            <div className='recipes-wrapper'>
             <div className="card-container" ref={listRef}>
                 <LumpiangShanghaiCard />
                 <LumpiangShanghaiCard />
@@ -28,7 +33,7 @@ const Appetizers = () => {
                 <LumpiangShanghaiCard />
                 <LumpiangShanghaiCard />
             </div>
-            
+            </div>
             <ArrowForwardIosOutlined onClick={() => handleArrowClick("right")}/> 
             </div>
         </div>
