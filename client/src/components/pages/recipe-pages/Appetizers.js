c6fd2724d9adc28df05b2ab5881b0fe5b6b34fbc
Stephanie Fajardo <stfajardo@gmail.com>
2022-05-15 14:11:24 -0400

    update to slider

--- client/src/components/pages/recipe-pages/Appetizers.js
+++ client/src/components/pages/recipe-pages/Appetizers.js
@@ -1,12 +1,26 @@
 import LumpiangShanghaiCard from "./recipe-cards/LumpiangShanghai-card";
 import { ArrowBackIosOutlined, ArrowForwardIosOutlined } from '@mui/icons-material';
+import { useRef } from 'react';
 
 const Appetizers = () => {
+   
+    const listRef = useRef();
+
+    const handleArrowClick = (direction) => {
+        let distance = listRef.current.getBoundingClientRect().x - 50;
+        if(direction === "right") {
+            listRef.current.style.transform = `translateX(${100 + distance}px)`;
+        }
+        if(direction === "left") {
+            listRef.current.style.transform = `translateX(${100 - distance}px)`;
+        }
+    }
+
     return (
         <div className='content-container'>
             <div className='recipes-wrapper'>
-            <ArrowBackIosOutlined /> 
-            <div className="card-container">
+            <ArrowBackIosOutlined onClick={() => handleArrowClick("left")} /> 
+            <div className="card-container" ref={listRef}>
                 <LumpiangShanghaiCard />
                 <LumpiangShanghaiCard />
                 <LumpiangShanghaiCard />
@@ -15,7 +29,7 @@ const Appetizers = () => {
                 <LumpiangShanghaiCard />
             </div>
             
-            <ArrowForwardIosOutlined /> 
+            <ArrowForwardIosOutlined onClick={() => handleArrowClick("right")}/> 
             </div>
         </div>
     )
